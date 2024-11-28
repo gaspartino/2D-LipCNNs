093d65f821fa15c294381d088ffc90803c8cb638
André Vinicius Santos de Lima <81827987+gaspartino@users.noreply.github.com>
2024-11-28 15:17:25 -0300

    Adding car dataset in dataset.py

--- dataset.py
+++ dataset.py
@@ -1,38 +1,64 @@
 from torchvision.datasets import MNIST
 from torch.utils.data import DataLoader
 from torchvision import transforms
-#from torchvision.datasets import VisionDataset
-#from torchvision.datasets.folder import default_loader
-#from torchvision.datasets.utils import extract_archive, check_integrity, download_url, verify_str_arg
-#from utils import load_obj
+import os
 
 def getDataLoader(config):
+    """
+    Seleciona o carregador com base no dataset configurado.
+    """
     loaders = {
-        'mnist': mnist_loaders
-    }[config.dataset]
-    return loaders(config)
+        'mnist': mnist_loaders,
+        'cars': cars_loaders
+    }
+    if config.dataset not in loaders:
+        raise ValueError(f"Dataset {config.dataset} não suportado.")
+    return loaders[config.dataset](config)
 
 def mnist_loaders(config):
+    """
+    Carregador do dataset MNIST.
+    """
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Resize((32, 32))
+        # Adicione transforms.Normalize se necessário
+    ])
 
-    trainLoader = DataLoader(MNIST('data',train=True,download=True,
-                        transform=transforms.Compose([
-                        transforms.ToTensor(),
-                        transforms.Resize((32, 32))
-                        #transforms.Normalize(mean, std)
-                        ])),
-                    batch_size=config.train_batch_size,
-                    shuffle=True, pin_memory=True)
+    trainLoader = DataLoader(
+        MNIST('data', train=True, download=True, transform=transform),
+        batch_size=config.train_batch_size,
+        shuffle=True,
+        pin_memory=True
+    )
 
-    testLoader = DataLoader(MNIST('data',
-                    train=False,
-                    transform=transforms.Compose([
-                        transforms.ToTensor(),
-                        transforms.Resize((32, 32))
-                        #transforms.Normalize(mean, std)
-                    ])),
-                batch_size=config.test_batch_size,
-                shuffle=False, pin_memory=True)
-    
+    testLoader = DataLoader(
+        MNIST('data', train=False, download=True, transform=transform),
+        batch_size=config.test_batch_size,
+        shuffle=False,
+        pin_memory=True
+    )
+
     return trainLoader, testLoader
-    
 
+def cars_loaders(config):
+    """
+    Carregador do dataset de carros.
+    """
+    dataset_path = '/kaggle/input/cars-image-dataset/Cars Dataset'
+    transform = transforms.Compose([
+        transforms.Resize((100, 100)),  # Altere o tamanho se necessário
+        transforms.ToTensor()
+        # Adicione transforms.Normalize se necessário
+    ])
+
+    train_path = os.path.join(dataset_path, 'Train')  # Subpasta Train
+    test_path = os.path.join(dataset_path, 'Test')    # Subpasta Test
+
+    train_dataset = ImageFolder(root=train_path, transform=transform)
+    test_dataset = ImageFolder(root=test_path, transform=transform)
+
+    trainLoader = DataLoader(train_dataset, batch_size=config.train_batch_size, shuffle=True, pin_memory=True)
+    testLoader = DataLoader(test_dataset, batch_size=config.test_batch_size, shuffle=False, pin_memory=True)
+
+    return trainLoader, testLoader
