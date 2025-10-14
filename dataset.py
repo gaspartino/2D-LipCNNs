2718c391e2838564014c28222148099b209700e4
André Vinicius Santos de Lima <81827987+gaspartino@users.noreply.github.com>
2025-10-14 03:36:09 -0300

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -1,148 +1,141 @@
-import torch
-import torch.nn.functional as F
+from torchvision.datasets import MNIST, CIFAR10, ImageFolder
+from torch.utils.data import DataLoader, random_split
 from torchvision import datasets, transforms
-from torch.utils.data.sampler import SubsetRandomSampler
-import numpy as np
-import os
 
-upper_limit, lower_limit = 1, 0
+def getDataLoader(config):
+    loaders = {
+        'mnist': mnist_loaders,
+        'imagefolder': imagefolder_loaders,
+        'cifar10': cifar10_loaders,
+        'vehicle': vehicle_loaders,
+        'lisa': lisa_loaders,
+        'bstl': bstl_loaders
+    }[config.dataset]
+    return loaders(config)
 
-cifar10_mean = (0.4914, 0.4822, 0.4465)
-cifar10_std = (0.2471, 0.2435, 0.2616)
-mu = torch.tensor(cifar10_mean).view(3,1,1).cuda()
-std = torch.tensor(cifar10_std).view(3,1,1).cuda()
-
-def normalize(X):
-    return (X - mu)/std
-
-def clamp(X, lower_limit, upper_limit):
-    return torch.max(torch.min(X, upper_limit), lower_limit)
-
-def get_loaders(dir_, batch_size, DATASET='CIFAR10'):
-    train_transform = transforms.Compose([
-        transforms.RandomCrop(32, padding=4),
-        transforms.RandomHorizontalFlip(),
-        transforms.ToTensor()
+def mnist_loaders(config):
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Resize((32, 32)),
+        #transforms.Normalize(mean, std)
     ])
-    test_transform = transforms.Compose([
-        transforms.ToTensor()
-    ])
-    num_workers = 8
-
-    if DATASET == 'CIFAR10':
-        train_dataset = datasets.CIFAR10(
-            dir_, train=True, transform=train_transform, download=True)
-        test_dataset = datasets.CIFAR10(
-            dir_, train=False, transform=test_transform, download=True)
-        train_dataset__ = datasets.CIFAR10(
-            dir_, train=True, transform=test_transform, download=True)
-    elif DATASET == 'CIFAR100':
-        train_dataset = datasets.CIFAR100(
-            dir_, train=True, transform=train_transform, download=True)
-        test_dataset = datasets.CIFAR100(
-            dir_, train=False, transform=test_transform, download=True)
-    elif DATASET == 'CUSTOM':
-        train_path = '/kaggle/input/flowers-dataset/train' 
-        test_path = '/kaggle/input/flowers-dataset/test' 
-        valid_path = '/kaggle/input/flowers-dataset/valid'    
-
-        train_dataset = datasets.ImageFolder(root=train_path, transform=train_transform)
-        test_dataset = datasets.ImageFolder(root=test_path, transform=test_transform)
-        train_dataset__ = datasets.ImageFolder(root=valid_path, transform=test_transform)
-
-    train_loader = torch.utils.data.DataLoader(
-        dataset=train_dataset,
-        batch_size=batch_size,
-        shuffle=True,
-        pin_memory=False,
-        num_workers=num_workers,
-    )
-    test_loader = torch.utils.data.DataLoader(
-        dataset=test_dataset,
-        batch_size=batch_size,
-        shuffle=False,
-        pin_memory=False,
-        num_workers=num_workers,
-    )
-    train_loader__ = torch.utils.data.DataLoader(
-        dataset=train_dataset__,
-        batch_size=batch_size,
-        shuffle=False,
-        pin_memory=False,
-        num_workers=num_workers,
-    )
-    return train_loader, test_loader, train_loader__, test_dataset
-
-
-
-def CW_loss(x, y):
-    x_sorted, ind_sorted = x.sort(dim=1)
-    ind = (ind_sorted[:, -1] == y).float()
     
-    loss_value = -(x[np.arange(x.shape[0]), y] - x_sorted[:, -2] * ind - x_sorted[:, -1] * (1. - ind))
-    return loss_value.mean()
+    trainLoader = DataLoader(MNIST('data', train=True, download=True, transform=transform),
+                             batch_size=config.train_batch_size, shuffle=True, pin_memory=True)
 
-def attack_pgd(model, X, y, epsilon, alpha, attack_iters, restarts, use_CWloss=False):
-    max_loss = torch.zeros(y.shape[0]).cuda()
-    max_delta = torch.zeros_like(X).cuda()
-    for _ in range(restarts):
-        delta = torch.zeros_like(X).cuda()
-        delta.uniform_(-epsilon, epsilon)
-        delta.data = clamp(delta, lower_limit - X, upper_limit - X)
-        delta.requires_grad = True
-        for _ in range(attack_iters):
-            output = model(normalize(X + delta))
-            index = torch.where(output.max(1)[1] == y)
-            if len(index[0]) == 0:
-                break
-            if use_CWloss:
-                loss = CW_loss(output, y)
-            else:
-                loss = F.cross_entropy(output, y)
-            loss.backward()
-            grad = delta.grad.detach()
-            d = delta[index[0], :, :, :]
-            g = grad[index[0], :, :, :]
-            d = torch.clamp(d + alpha * torch.sign(g), -epsilon, epsilon)
-            d = clamp(d, lower_limit - X[index[0], :, :, :], upper_limit - X[index[0], :, :, :])
-            delta.data[index[0], :, :, :] = d
-            delta.grad.zero_()
-        all_loss = F.cross_entropy(model(normalize(X + delta)), y, reduction='none').detach()
-        max_delta[all_loss >= max_loss] = delta.detach()[all_loss >= max_loss]
-        max_loss = torch.max(max_loss, all_loss)
-    return max_delta
+    testLoader = DataLoader(MNIST('data', train=False, download=True, transform=transform),
+                            batch_size=config.test_batch_size, shuffle=False, pin_memory=True)
+    
+    return trainLoader, testLoader
 
+def cifar10_loaders(config):
+    transform = transforms.Compose([
+        transforms.Resize((config.img_size, config.img_size)),
+        transforms.ToTensor(),
+        #transforms.Normalize(mean, std)
+    ])
+    
+    trainLoader = DataLoader(CIFAR10('data', train=True, download=True, transform=transform),
+                             batch_size=config.train_batch_size, shuffle=True, pin_memory=True)
 
-def evaluate_pgd(test_loader, model, attack_iters, restarts, eps=8, step=2, use_CWloss=False):
-    epsilon = eps / 255.
-    alpha = step / 255.
-    pgd_loss = 0
-    pgd_acc = 0
-    n = 0
-    model.eval()
-    for i, (X, y) in enumerate(test_loader):
-        X, y = X.cuda(), y.cuda()
-        pgd_delta = attack_pgd(model, X, y, epsilon, alpha, attack_iters, restarts, use_CWloss=use_CWloss)
-        with torch.no_grad():
-            output = model(normalize(X + pgd_delta))
-            loss = F.cross_entropy(output, y)
-            pgd_loss += loss.item() * y.size(0)
-            pgd_acc += (output.max(1)[1] == y).sum().item()
-            n += y.size(0)
-    return pgd_loss/n, pgd_acc/n
+    testLoader = DataLoader(CIFAR10('data', train=False, download=True, transform=transform),
+                            batch_size=config.test_batch_size, shuffle=False, pin_memory=True)
 
+    return trainLoader, testLoader
 
-def evaluate_standard(test_loader, model):
-    test_loss = 0
-    test_acc = 0
-    n = 0
-    model.eval()
-    with torch.no_grad():
-        for i, (X, y) in enumerate(test_loader):
-            X, y = X.cuda(), y.cuda()
-            output = model(normalize(X))
-            loss = F.cross_entropy(output, y)
-            test_loss += loss.item() * y.size(0)
-            test_acc += (output.max(1)[1] == y).sum().item()
-            n += y.size(0)
-    return test_loss/n, test_acc/n
+def imagefolder_loaders(config):
+    transform = transforms.Compose([
+        transforms.Resize((config.img_size, config.img_size)),
+        transforms.ToTensor(),
+    ])
+    
+    data_dir = '/kaggle/input/cars-image-dataset/Cars Dataset'
+    train_dir = f"{data_dir}/train"
+    test_dir = f"{data_dir}/test"
+
+    train_data = ImageFolder(root=train_dir, transform=transform)
+    test_data = ImageFolder(root=test_dir, transform=transform)
+
+    trainLoader = DataLoader(train_data, batch_size=config.train_batch_size,
+                             shuffle=True, pin_memory=True)
+    testLoader = DataLoader(test_data, batch_size=config.test_batch_size,
+                            shuffle=False, pin_memory=True)
+
+    return trainLoader, testLoader
+
+import torch
+
+def lisa_loaders(config):
+    path = '/kaggle/input/cropped-lisa-traffic-light-dataset'
+    train_dir = f"{path}/cropped_lisa_1/train_1"
+    val_dir = f"{path}/cropped_lisa_1/val_1"
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    transform_list = [transforms.Resize((dim, 32)), transforms.ToTensor()]
+
+    if config.normalize:
+        transform_list.append(
+            transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                 std=[0.229, 0.224, 0.225])
+        )
+
+    transform = transforms.Compose(transform_list)
+    
+    train_dataset = ImageFolder(train_dir, transform=transform)
+    test_dataset = ImageFolder(val_dir, transform=transform)
+
+    train_loader = DataLoader(train_dataset, batch_size=config.train_batch_size, shuffle=True, num_workers=2)
+    test_loader = DataLoader(test_dataset, batch_size=config.test_batch_size, shuffle=False, num_workers=2)
+    return train_loader, test_loader
+
+def vehicle_loaders(config):
+    transform = transforms.Compose([
+        transforms.Resize((config.img_size, config.img_size)),
+        transforms.ToTensor(),
+        #transforms.Normalize(
+        #    mean=[0.485, 0.456, 0.406],  # ImageNet mean
+        #    std=[0.229, 0.224, 0.225]    # ImageNet std
+        #)
+    ])
+    test_split = 0.2
+    data_dir = '/kaggle/input/vehicle-detection-image-set/data'
+
+    full_dataset = datasets.ImageFolder(root=data_dir, transform=transform)
+    
+    total_size = len(full_dataset)
+    test_size = int(total_size * test_split)
+    train_size = total_size - test_size
+    
+    train_dataset, test_dataset = random_split(full_dataset, [train_size, test_size])
+    
+    trainLoader = DataLoader(train_dataset, batch_size=config.train_batch_size, shuffle=True)
+    testLoader = DataLoader(test_dataset, batch_size=config.test_batch_size, shuffle=False)
+    
+    return trainLoader, testLoader
+
+def bstl_loaders(config):
+    train_dir = "/kaggle/input/bstl-dataset/train"
+    test_dir = "/kaggle/input/bstl-dataset/test"
+
+    transform_list = [transforms.Resize((dim, 32)), transforms.ToTensor()]
+
+    if config.normalize:
+        transform_list.append(
+            transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                 std=[0.229, 0.224, 0.225])
+        )
+
+    transform = transforms.Compose(transform_list)
+
+    train_dataset = datasets.ImageFolder(root=train_dir, transform=transform)
+    test_dataset = datasets.ImageFolder(root=test_dir, transform=transform)
+
+    train_loader = DataLoader(train_dataset, batch_size=config.train_batch_size, shuffle=True, num_workers=2)
+    test_loader = DataLoader(test_dataset, batch_size=1000, shuffle=False, num_workers=4)
+
+    print(f"Número de imagens em train: {len(train_dataset)}")
+    print(f"Número de imagens em test: {len(test_dataset)}")
+    print(f"Classes: {train_dataset.classes}")
+
+    return train_loader, test_loader
