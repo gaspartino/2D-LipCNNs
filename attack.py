737bb7e6ecf221a82bb926ec6d9649e3d4127b60
André Vinicius Santos de Lima <81827987+gaspartino@users.noreply.github.com>
2026-06-11 14:45:50 -0300

    Update attack.py

--- attack.py
+++ attack.py
@@ -51,11 +51,7 @@ def evaluate_model(model, dataset_loader, attack=None, attack_name="Clean", devi
     recall = recall_score(y_true_all, y_pred_all, average='macro', zero_division=0)
     f1 = f1_score(y_true_all, y_pred_all, average='macro', zero_division=0)
 
-    print(f"\n=== {attack_name} RESULTS ===")
-    print(f"Accuracy:  {acc * 100:.2f}%")
-    print(f"Precision: {precision * 100:.2f}%")
-    print(f"Recall:    {recall * 100:.2f}%")
-    print(f"F1-score:  {f1 * 100:.2f}%\n")
+    print(f"{attack_name} | Accuracy: {acc * 100:.2f}%, Precision: {precision:.4f}%, Recall: {recall:.4f}%, F1-score: {f1:.4f}%")
 
     return acc, precision, recall, f1
 
@@ -69,7 +65,7 @@ def accuracy_FGSM(model, dataset_loader, eps, device, normalize):
     if normalize:
         attack.set_normalization_used(mean=mean, std=std)
 
-    return evaluate_model(model, dataset_loader, attack, f"FGSM (ε={eps})", device)
+    return evaluate_model(model, dataset_loader, attack, f"FGSM (ε={eps:.2f})", device)
 
 def accuracy_PGD(model, dataset_loader, eps, device, normalize):
     attack = torchattacks.PGD(model, eps=eps)   
@@ -78,7 +74,7 @@ def accuracy_PGD(model, dataset_loader, eps, device, normalize):
     if normalize:
         attack.set_normalization_used(mean=mean, std=std)
 
-    return evaluate_model(model, dataset_loader, attack, f"PGD (ε={eps})", device)
+    return evaluate_model(model, dataset_loader, attack, f"PGD (ε={eps:.2f})", device)
 
 def accuracy_MIM(model, dataset_loader, eps, device, normalize):
     attack = torchattacks.MIFGSM(model, eps=eps) 
@@ -87,7 +83,7 @@ def accuracy_MIM(model, dataset_loader, eps, device, normalize):
     if normalize:
         attack.set_normalization_used(mean=mean, std=std)
 
-    return evaluate_model(model, dataset_loader, attack, f"MIM (ε={eps})", device)
+    return evaluate_model(model, dataset_loader, attack, f"MIM (ε={eps:.2f})", device)
     
 def accuracy_AutoAttack(model, dataset_loader, num_classes, eps, device, normalize):
     attack = torchattacks.AutoAttack(model, eps=eps, n_classes=num_classes)
@@ -96,7 +92,7 @@ def accuracy_AutoAttack(model, dataset_loader, num_classes, eps, device, normali
     if normalize:
         attack.set_normalization_used(mean=mean, std=std)
 
-    return evaluate_model(model, dataset_loader, attack, f"AutoAttack (ε={eps})", device)
+    return evaluate_model(model, dataset_loader, attack, f"AutoAttack (ε={eps:.2f})", device)
 
 def PGDL2_attack(config):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
